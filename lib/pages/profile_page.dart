3d353b11473794811031858953ac6d3b58b48e2c
Minoli Bhagya <mbhagyagunathilake@gmail.com>
2026-09-23 19:40:34 +0530

    Changes added to profile page

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter_workout_planner/constants/colors.dart';
 import 'package:flutter_workout_planner/constants/responsive.dart';
 import 'package:flutter_workout_planner/data/user_data.dart';
 import 'package:flutter_workout_planner/widgets/home_page/progress_card.dart';
+import 'package:flutter_workout_planner/widgets/profile_page/added_exercise_card.dart';
+import 'package:flutter_workout_planner/widgets/reusable/heading_text.dart';
 import 'package:intl/intl.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -26,37 +28,68 @@ class _ProfilePageState extends State<ProfilePage> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
-            padding: const EdgeInsets.all(kDefaultPadding),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "$formattedDate $formattedDay",
-                  style: TextStyle(
-                    color: kMainSubColor,
-                    fontSize: 16,
-                    fontWeight: FontWeight.normal,
-                  ),
+          padding: const EdgeInsets.all(kDefaultPadding),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "$formattedDate $formattedDay",
+                style: TextStyle(
+                  color: kMainSubColor,
+                  fontSize: 16,
+                  fontWeight: FontWeight.normal,
                 ),
+              ),
 
-                Text(
-                  "Hello, $userName",
-                  style: TextStyle(
-                    color: kMainBlackColor,
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                  ),
+              Text(
+                "Hello, $userName",
+                style: TextStyle(
+                  color: kMainBlackColor,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
                 ),
+              ),
 
-                SizedBox(height: 30),
+              SizedBox(height: 30),
 
-                ProgressCard(progressValue: 0.08, total: 100),
+              ProgressCard(progressValue: 0.08, total: 100),
 
-                SizedBox(height: 20),
-                
-              ],
-            ),
+              SizedBox(height: 20),
+
+              HeadingText(title: "Your Exercies"),
+
+              SizedBox(height: 20),
+
+              // List of added exercises
+              Column(
+                children: user.exerciseList.map((exercise) {
+                  return AddedExerciseCard(
+                    yourExerciseName: exercise.exerciseName,
+                    yourExerciseImageUrl: exercise.exersiceImageUrl,
+                    buttonText: 'Mark as Done',
+                  );
+                }).toList(),
+              ),
+
+              SizedBox(height: 20),
+
+              HeadingText(title: "Your Equipments"),
+
+              SizedBox(height: 20),
+
+              // List of added exercises
+              Column(
+                children: user.equipmentList.map((equipment) {
+                  return AddedExerciseCard(
+                    yourExerciseName: equipment.euipqmentName,
+                    yourExerciseImageUrl: equipment.equipmentUrl,
+                    buttonText: 'Mark as Handovered',
+                  );
+                }).toList(),
+              ),
+            ],
           ),
+        ),
       ),
     );
   }
---
+++ lib/widgets/profile_page/added_exercise_card.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_workout_planner/constants/colors.dart';
+
+class AddedExerciseCard extends StatefulWidget {
+  final String yourExerciseName;
+  final String yourExerciseImageUrl;
+  final String buttonText;
+  const AddedExerciseCard({
+    super.key,
+    required this.yourExerciseName,
+    required this.yourExerciseImageUrl, required this.buttonText,
+  });
+
+  @override
+  State<AddedExerciseCard> createState() => _AddedExerciseCardState();
+}
+
+class _AddedExerciseCardState extends State<AddedExerciseCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      margin: const EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+        color: Color(0xFFF5F5F5),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(
+            width: 150,
+            child: Text(
+              widget.yourExerciseName,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Image.asset(
+            widget.yourExerciseImageUrl,
+            width: 50,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            width: 3,
+            height: 45,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(1.5),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // Handle button press
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kMainBlueColor,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(100),
+              ),
+            ),
+            child: Text(
+              widget.buttonText,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
