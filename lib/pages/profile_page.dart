5ee7652c88bb0ec8ce7865b96e79b8539ba98bed
Minoli Bhagya <mbhagyagunathilake@gmail.com>
2026-09-23 18:23:07 +0530

    Changes added to profile page

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_workout_planner/constants/colors.dart';
+import 'package:flutter_workout_planner/constants/responsive.dart';
+import 'package:flutter_workout_planner/data/user_data.dart';
+import 'package:flutter_workout_planner/widgets/home_page/progress_card.dart';
+import 'package:intl/intl.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -8,11 +13,50 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final DateFormat formatter = DateFormat("EEEE, MMMM");
+  final DateFormat dayFormat = DateFormat("dd");
+
   @override
   Widget build(BuildContext context) {
+    DateTime now = DateTime.now();
+    String formattedDate = formatter.format(now);
+    String formattedDay = dayFormat.format(now);
+    String userName = user.fullName;
+
     return Scaffold(
       body: SingleChildScrollView(
-        child: Text("Profile page"),
+        child: Padding(
+            padding: const EdgeInsets.all(kDefaultPadding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "$formattedDate $formattedDay",
+                  style: TextStyle(
+                    color: kMainSubColor,
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+
+                Text(
+                  "Hello, $userName",
+                  style: TextStyle(
+                    color: kMainBlackColor,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                SizedBox(height: 30),
+
+                ProgressCard(progressValue: 0.08, total: 100),
+
+                SizedBox(height: 20),
+                
+              ],
+            ),
+          ),
       ),
     );
   }
