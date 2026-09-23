cb2e3e3eaa884b0414a50711881ed290c989ae96
Minoli Bhagya <mbhagyagunathilake@gmail.com>
2026-09-23 13:31:32 +0530

    Changes added to Add new page

--- lib/pages/add_new_page.dart
+++ lib/pages/add_new_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_workout_planner/constants/colors.dart';
+import 'package:flutter_workout_planner/constants/responsive.dart';
+import 'package:flutter_workout_planner/data/user_data.dart';
+import 'package:flutter_workout_planner/widgets/reusable/heading_text.dart';
 
 class AddNewPage extends StatefulWidget {
   const AddNewPage({super.key});
@@ -8,11 +12,43 @@ class AddNewPage extends StatefulWidget {
 }
 
 class _AddNewPageState extends State<AddNewPage> {
+  final userData = user;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Text("Add new page"),
+        child: Padding(
+          padding: const EdgeInsets.all(kDefaultPadding),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Hello, ${userData.fullName}",
+                style: const TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+
+              SizedBox(height: 15),
+
+              Text(
+                "Lets Add Some Workouts and Equipment for today!",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: kMainBlueColor,
+                  fontWeight: FontWeight.w500
+                ),
+              ),
+
+              SizedBox(height: 5),
+
+              HeadingText(title: "All Exercises"),
+            ],
+          ),
+        ),
       ),
     );
   }
